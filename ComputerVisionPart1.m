87ce4cdf690c3ed087f9fe08a3bd099d908e02ae
Christian Velez <m236510@usna.edu>
2022-01-25 14:26:39 -0500

    initial Velez

--- ComputerVisionPart1.m
+++ ComputerVisionPart1.m
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 cam1 = webcam(2);
 img = snapshot(cam1);
 figure(1)
@@ -15,3 +16,22 @@ while true
  %Text = 'Frames per second: %f\n';
 %fprintf(Text,FPS)
 end
+=======
+cam1 = webcam(2);
+img = snapshot(cam1);
+figure(1)
+imshow(img)
+hold on
+xline(320,'r')
+yline(240,'r')
+while true
+ %tic
+ img = snapshot(cam1); 
+ imshow(img)
+ drawnow
+ %toc
+ %FPS = 1/(tic-toc);
+ %Text = 'Frames per second: %f\n';
+%fprintf(Text,FPS)
+end
+>>>>>>> 146fb38 (initial Velez)
