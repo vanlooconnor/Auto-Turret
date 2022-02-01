f713c698e44de5dbcd290e25583933be0ebd51bc
Connor Van Loo <m236450@usna.edu>
2022-02-01 15:12:49 -0500

    Got it to work. Had to remake a filter. Also if we want to do different color targets it should be pretty easy to do with some if statements and a few new functions.

--- ComputerVisionPart1.m
+++ ComputerVisionPart1.m
@@ -1,21 +1,22 @@
 clear;close all;clc;
-minArea = 50;
+minArea = 500;
 maxArea = 480*640;
-minEcc = 0;
-maxEcc = 1;
+minEcc = .05;
+maxEcc = .95;
 cam1 = webcam(1);
+pause(1)
 img = snapshot(cam1);
 
-figure(1)
+%figure(1)
 imshow(img)
-hold on;
+hold on
 aimpoint = plot(320,240,'+m','MarkerSize',15);
-
 while true
+    clear targetpoint figure(1)
  img = snapshot(cam1); 
  imshow(img);
- hold on 
  aimpoint = plot(320,240,'+m','MarkerSize',15);
- [statsTargets,binTargets,binThreshold] = targetThreshold(img,minArea,maxArea,minEcc,maxEcc);
- drawnow
+ [statsTargets,binTargets,bin] = targetThresholdYellow(img,minArea,maxArea,minEcc,maxEcc);
+ targetpoint = plot(statsTargets.Centroid(1),statsTargets.Centroid(2),'+r','MarkerSize',15);
+  drawnow
 end
