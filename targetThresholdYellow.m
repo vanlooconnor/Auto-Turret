558b63a93ce46615d873f3084dc5ce827b7d27cf
Connor Van Loo <m236450@usna.edu>
2022-02-03 21:23:25 -0500

    I had to create a new filter to allow more than 1 target to be inbounds. The mat file is everything that was in my workspace when creating the intrinsic matrix. Added just incase it was needed later on.

--- targetThresholdYellow.m
+++ targetThresholdYellow.m
@@ -1,15 +1,19 @@
-function [statsTargets,binTargets,bin] = targetThresholdYellow(img,minArea,maxArea,minEcc,maxEcc)
+function [statsTargets,binTargets,bin] = targetThresholdYellow2(img,minArea,maxArea,minEcc,maxEcc)
 
-bin = createMaskYellow(img); %Performs color thresholding
+bin = createMaskYellow2(img); %Performs color thresholding
 binFill = imfill(bin,'holes'); %Filling Holes in the resultant binary
 binMinArea = bwareaopen(binFill,minArea); %Imposes a minimum area threshold
 [lbl,n] = bwlabel(binMinArea);% Labels connected componets 
-stats = regionprops(lbl,'area','centroid','eccentricity'); %Calculates region properties of connected components
+stats = regionprops(lbl,'area','centroid','eccentricity','BoundingBox'); %Calculates region properties of connected components
 idx = find([stats.Area] <= maxArea &...
     [stats.Eccentricity] >= minEcc &...
     [stats.Eccentricity] <= maxEcc); %sets minimum and maximum threshold values for area and eccentricity.
 statsTargets = stats(idx);%Imposes remaining property thresholding
-for i = idx
-binTargets{i} = lbl == i;
-end
+    if idx ~= 0
+        for i = 1:idx
+            binTargets{i} = lbl==i;
+        end
+    else
+       binTargets = 0;   
+    end
 end
